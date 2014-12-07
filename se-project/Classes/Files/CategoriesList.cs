5250c9dbac30a5cd42e85df11387f6d922a221f8
Michał Witas <bolek117@interia.pl>
2014-12-07 19:43:50 +0100

    CategoriesList class completed

--- se-project/Classes/Files/CategoriesList.cs
+++ se-project/Classes/Files/CategoriesList.cs
@@ -7,7 +7,119 @@ using se_project.Classes.Files;
 
 namespace se_project.Classes.Files
 {
+    /// <summary>
+    /// Class storing categories list.
+    /// </summary>
     class CategoriesList
     {
+        /// <summary>
+        /// List storing categories list.
+        /// </summary>
+        protected List<FileCategory> categoriesList;
+
+        /// <summary>
+        /// Internal value storing actual position in the list.
+        /// </summary>
+        private List<FileCategory>.Enumerator position;
+
+        /// <summary>
+        /// Default constructor. Initializes internal variables.
+        /// </summary>
+        CategoriesList()
+        {
+            position = categoriesList.GetEnumerator();
+        }
+
+        /// <summary>
+        /// Returns categories list.
+        /// </summary>
+        /// <returns>Categories list.</returns>
+        List<FileCategory> GetCategoriesList()
+        {
+            return categoriesList;
+        }
+
+        /// <summary>
+        /// Clears category list.
+        /// </summary>
+        /// <returns>True if cleared, else unspecified.</returns>
+        bool ClearCategoriesList()
+        {
+            categoriesList.Clear();
+            return true;
+        }
+
+        /// <summary>
+        /// Returns category identified by ID.
+        /// </summary>
+        /// <param name="id">ID of category to return by method.</param>
+        /// <returns>Category identified by ID or null if category not found.</returns>
+        FileCategory GetCategoryById(int id)
+        {
+            foreach(FileCategory fc in categoriesList)
+            {
+                if (fc.GetId() == id)
+                {
+                    return fc;
+                }
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Returns next category from the list.
+        /// </summary>
+        /// <returns>Next category or null if next category not found.</returns>
+        FileCategory GetNextCategory()
+        {
+            if (position.MoveNext())
+            {
+                return position.Current;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Adds category to list.
+        /// </summary>
+        /// <param name="category">Category to add.</param>
+        /// <returns>True if added, else false.</returns>
+        bool AddCategory(FileCategory category)
+        {
+            categoriesList.Add(category);
+            return true;
+        }
+
+        /// <summary>
+        /// Removes categoryu from categories list.
+        /// </summary>
+        /// <param name="category">Category to delete.</param>
+        /// <returns>True if removed, else false.</returns>
+        bool RemoveCategory(FileCategory category)
+        {
+            return categoriesList.Remove(category);
+        }
+
+        /// <summary>
+        /// Removes category from categories list identified by <paramref name="id"/>.
+        /// </summary>
+        /// <param name="id">ID of the category to remove.</param>
+        /// <returns>True if removed. False if cannot remove or category not found.</returns>
+        bool RemoveCategory(int id)
+        {
+            foreach (FileCategory fc in categoriesList)
+            {
+                if (fc.GetId() == id)
+                {
+                    return categoriesList.Remove(fc);
+                }
+            }
+
+            return false;
+        }
     }
 }
