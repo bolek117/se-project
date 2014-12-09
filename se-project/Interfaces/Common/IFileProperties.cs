ebca7c2af4d733bc856d2ce068283cddf9b10f7b
Michał Witas <bolek117@interia.pl>
2014-12-09 21:44:28 +0100

    Interface filling continued

--- se-project/Interfaces/Common/IFileProperties.cs
+++ se-project/Interfaces/Common/IFileProperties.cs
@@ -3,9 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using se_project.Interfaces.Point;
+using se_project.Interfaces.Files;
 namespace se_project.Interfaces.Common
 {
     interface IFileProperties
     {
+        /// <summary>
+        /// Gets whole list
+        /// </summary>
+        /// <returns></returns>
+        IPointsList GetPointsList();
+
+        /// <summary>
+        /// Sets list to given <paramref name="pointList"/>
+        /// </summary>
+        /// <param name="pointList"></param>
+        /// <returns></returns>
+        bool SetPointList(IPointsList pointList);
+
+        /// <summary>
+        /// Gets actual file
+        /// </summary>
+        /// <returns></returns>
+        IFile GetFile();
+
+        /// <summary>
+        /// Sets actual file
+        /// </summary>
+        /// <returns></returns>
+        bool SetFile(IFile file);
     }
 }
--- se-project/Interfaces/Files/ICategoriesList.cs
+++ se-project/Interfaces/Files/ICategoriesList.cs
@@ -7,5 +7,50 @@ namespace se_project.Interfaces.Files
 {
     interface ICategoriesList
     {
+        /// <summary>
+        /// Returns categories list.
+        /// </summary>
+        /// <returns>Categories list.</returns>
+        List<IFileCategory> GetCategoriesList();
+
+        /// <summary>
+        /// Clears category list.
+        /// </summary>
+        /// <returns>True if cleared, else unspecified.</returns>
+        bool ClearCategoriesList();
+
+        /// <summary>
+        /// Returns category identified by ID.
+        /// </summary>
+        /// <param name="id">ID of category to return by method.</param>
+        /// <returns>Category identified by ID or null if category not found.</returns>
+        IFileCategory GetCategoryById(int id);
+
+        /// <summary>
+        /// Returns next category from the list.
+        /// </summary>
+        /// <returns>Next category or null if next category not found.</returns>
+        IFileCategory GetNextCategory();
+
+        /// <summary>
+        /// Adds category to list.
+        /// </summary>
+        /// <param name="category">Category to add.</param>
+        /// <returns>True if added, else false.</returns>
+        bool AddCategory(IFileCategory category);
+
+        /// <summary>
+        /// Removes categoryu from categories list.
+        /// </summary>
+        /// <param name="category">Category to delete.</param>
+        /// <returns>True if removed, else false.</returns>
+        bool RemoveCategory(IFileCategory category);
+
+        /// <summary>
+        /// Removes category from categories list identified by <paramref name="id"/>.
+        /// </summary>
+        /// <param name="id">ID of the category to remove.</param>
+        /// <returns>True if removed. False if cannot remove or category not found.</returns>
+        bool RemoveCategory(int id);
     }
 }
