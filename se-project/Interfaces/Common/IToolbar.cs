0cbfafbf8499a2dd02177932bd84fa9a12d92a09
Michał Witas <bolek117@interia.pl>
2014-12-09 22:50:13 +0100

    Missing toolbar method added

--- se-project/Interfaces/Common/IToolbar.cs
+++ se-project/Interfaces/Common/IToolbar.cs
@@ -7,6 +7,11 @@ namespace se_project.Interfaces.Common
 {
     interface IToolbar
     {
-        
+        /// <summary>
+        /// Executes action associated with <paramref name="buttonId"/>
+        /// </summary>
+        /// <param name="buttonId">ID of button for which action should be performed</param>
+        /// <returns>True if success, else false</returns>
+        bool ExecuteButtonAction(int buttonId);
     }
 }
