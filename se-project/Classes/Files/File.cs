0de7e00b45dfdba6a0d2a4d8e455c7c606278909
Zielak <zielakmaster@gmail.com>
2014-12-14 20:34:46 +0100

    In CategoriesList class method name changed from GetCategoryById to GetCategory; in File classes null implementation of related interfaces

--- se-project/Classes/Files/File.cs
+++ se-project/Classes/Files/File.cs
@@ -2,10 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using se_project.Interfaces.Files;
 
 namespace se_project.Classes.Files
 {
-    class File
+    class File : IFile
     {
+        int id;
+        int size;
+        string path;
+
+        public int GetId()
+        {
+            return id;
+        }
+
+        public string GetPath()
+        {
+            return path;
+        }
+
+        public int GetSize()
+        {
+            return size;
+        }
     }
 }
--- se-project/Classes/Files/FileList.cs
+++ se-project/Classes/Files/FileList.cs
@@ -2,10 +2,66 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using se_project.Interfaces.Files;
 
 namespace se_project.Classes.Files
 {
-    class FileList
+    class FileList : IFileList
     {
+
+        public int GetOrder(IFile file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetOrder(int fileId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SetOrder(int fileId, int order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SetOrder(IFile file, int order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IFile GetFile(int fileId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IFile GetNextFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ResetPosition()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AddFile(IFile file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveFile(IFile file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveFile(int file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ClearFileList()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
