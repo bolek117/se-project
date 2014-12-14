5c67965a2e0016e133ec1cbeb61915881efeaa12
Lukkub <lukischagi@wp.pl>
2014-12-14 15:41:21 +0100

    Toolbar
    Main Toolbar logic

--- se-project/Classes/Common/Toolbar.cs
+++ se-project/Classes/Common/Toolbar.cs
@@ -2,10 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using se_project.Interfaces.Common;
 
 namespace se_project.Classes.Common
 {
-    class Toolbar
+    public class Toolbar : IToolbar
     {
+       bool IToolbar.ExecuteButtonAction(int buttonId)
+        {
+            switch (buttonId)
+            {
+                case 1:
+                    executeFunction1();
+                    return true;
+                case 2:
+                    executeFunction2();
+                    return true;
+                default:
+                    badButton();
+                    return false;
+            }
+        }
+
+        private void executeFunction1() {
+            Console.WriteLine("Case 1");    
+        }
+
+        private void executeFunction2()
+        {
+            Console.WriteLine("Case 2");
+        }
+
+        private void badButton()
+        {
+            Console.WriteLine("Wrong buttonId !");
+        }
     }
 }
