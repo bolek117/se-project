8c47aa310b725904b7ad469402dfc069cfc697a1
Michał Witas <bolek117@interia.pl>
2014-12-14 19:03:25 +0100

    Modifiers public added to IFileCategory interface methods

--- se-project/Interfaces/Files/IFileCategory.cs
+++ se-project/Interfaces/Files/IFileCategory.cs
@@ -11,33 +11,33 @@ namespace se_project.Interfaces.Files
         /// Returns unique identifier of the category
         /// </summary>
         /// <returns>Unique identifier of the category</returns>
-        int GetId();
+        public int GetId();
 
         /// <summary>
         /// Sets identifier of the category.
         /// </summary>
         /// <param name="id">Identifier of the category.</param>
         /// <returns>True if success, else false.</returns>
-        bool SetId(int id);
+        public bool SetId(int id);
 
 
         /// <summary>
         /// Retrurns name of the category.
         /// </summary>
         /// <returns>Name of the category.</returns>
-        String GetName();
+        public String GetName();
 
         /// <summary>
         /// Sets name of the category.
         /// </summary>
         /// <param name="name">New name for category.</param>
         /// <returns>True if success, else false.</returns>
-        bool SetName(String name);
+        public bool SetName(String name);
 
         /// <summary>
         /// Returns list of files.
         /// </summary>
         /// <returns>List of files.</returns>
-        IFileList GetFilesList();
+        public IFileList GetFilesList();
     }
 }
