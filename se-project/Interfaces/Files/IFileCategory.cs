b6acb2258eb2134aadbe04de82a2441f1c33197d
Michał Witas <bolek117@interia.pl>
2014-12-14 19:16:28 +0100

    Modifier public removed from interfaces

--- se-project/Interfaces/Files/IFileCategory.cs
+++ se-project/Interfaces/Files/IFileCategory.cs
@@ -11,33 +11,32 @@ namespace se_project.Interfaces.Files
         /// Returns unique identifier of the category
         /// </summary>
         /// <returns>Unique identifier of the category</returns>
-        public int GetId();
+        int GetId();
 
         /// <summary>
         /// Sets identifier of the category.
         /// </summary>
         /// <param name="id">Identifier of the category.</param>
         /// <returns>True if success, else false.</returns>
-        public bool SetId(int id);
-
+        bool SetId(int id);
 
         /// <summary>
         /// Retrurns name of the category.
         /// </summary>
         /// <returns>Name of the category.</returns>
-        public String GetName();
+        String GetName();
 
         /// <summary>
         /// Sets name of the category.
         /// </summary>
         /// <param name="name">New name for category.</param>
         /// <returns>True if success, else false.</returns>
-        public bool SetName(String name);
+        bool SetName(String name);
 
         /// <summary>
         /// Returns list of files.
         /// </summary>
         /// <returns>List of files.</returns>
-        public IFileList GetFilesList();
+        IFileList GetFilesList();
     }
 }
